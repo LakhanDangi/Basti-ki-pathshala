0763a05a9d51864fdd003b19c8e905fd9a48adb4
Lakhan Dangi <lakhand9799@gmail.com>
2025-08-06 22:35:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+    // Mobile menu toggle
+    document.querySelector('.menu-toggle').addEventListener('click', () => {
+      document.querySelector('.nav-links').classList.toggle('active');
+    });
